b6f4a5173717f65a65a17d360c2a43a3b7151e29
miranda-liu <47536718+miranda-liu@users.noreply.github.com>
2024-01-17 10:55:51 -0800

    Advanced CSS + Tailwind CSS

---
+++ Lessons/Advanced-CSS/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// module.exports = {
+//   content: ["./src/**/*.{html,js}", "./public/*.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
